ba0005c3d5b3ab531fbb87a6132391ee9c11ea3e
carlist <carlist@147.228.48.63>
2023-11-21 10:22:46 +0100

    AUTOPLOT plots a spectrum or spectra computed by the autofft function.

--- +utilities/autoPlot.m
+++ +utilities/autoPlot.m
@@ -1,71 +1,197 @@
 function autoPlot(setup, s, f, t)
+%AUTOPLOT plots a spectrum or spectra computed by the autofft function.
+%
+% Copyright (c) 2023, Lubos Smolik
+% v1.0.0beta (build 7. 11. 2023)
+%
+% This code is published under BSD-3-Clause License.
+%
+% autoPlot(setup, s, f)
+% autoPlot(setup, s, f, t)
+%
+% autoPlot(setup, s, f) plots a spectrum or spectra stored in s assuming
+%   frequencies stored in a vector f. Visualisation parameters are
+%   determined by PlotLayout and Unit fields in a structure array setup.
+%   
+% autoPlot(setup, s, f, t) plots a spectrogram or spectrograms stored in s
+%   assuming frequencies stored in a vector f and time stamps stored in a
+%   vector t. Visualisation parameters are determined by PlotLayout and
+%   Unit fields in a structure array setup.
+%
+% --- TO DO: Detailed explanation of field values ---
 
-    % Validate number of input arguments
-    narginchk(3,4)
-    
-    % Change plot layout from stacked to tiled for spectrograms
-    if setup.PlotLayout == "stacked" && setup.Averaging == "none"
-       setup.PlotLayout = "tiled";
-    end
-    
-    % Determine spectral unit and set y-axis label
+% Validate number of input arguments
+narginchk(3,4)
 
-    % Plot data
-    switch setup.PlotLayout
-        case "stacked"
-            % Stacked plots
-	        fig = figure;
-		    ax  = initAxes(fig, sLabel);
-		    legend(ax);
-		    
-            for col = 1:size(s,2)
-                plot(ax, f, s(:, col), DisplayName = "Signal " + col);
-            end
-		    
-        case "tiled"
-            % Tiled plots
-            fig = figure;
-    
-            % Use a tiled layout (R2019a or newer)
-            try
-                tiledlayout(fig, "flow")
-    
-            % Use subplot if tiledlayout does not exist (R2018b or older)
-            catch
-    
-            end
-       
-        otherwise
-            for col = 1:size(s,2)
-                % Initialize figure
-                fig = figure;
-                
-                % Plot spectra as plots and spectrograms as surfaces
-			    if setup.Averaging == "none"
-			        ax = initAxes(fig, "Time (s)");
-			        surface(ax, f, t, squeeze(s(:, :, col)), ...
-                            DisplayName = "Signal " + col, ...
-                            EdgeColor = "none", FaceColor = "interp");
-                    cbar = colorbar; 
-                    title(cbar, slabel);
+%% Validate inputs
+% Check if a "Unit" field exists and if it is nonempty
+if ~isfield(setup, "Unit") || strtrim(setup.Unit) == ""
+    setup.Unit = "MU";
+end
+
+% Check if a "Averaging" field exists and if it is nonempty
+if ~isfield(setup, "Averaging") || strtrim(setup.Averaging) == ""
+    setup.Averaging = "linear";
+end
+
+% Check if a "PlotLayout" field exists and if it is valid
+if ~isfield(setup, "PlotLayout") || ...
+   (setup.PlotLayout == "stacked" && setup.Averaging == "none")
+    setup.PlotLayout = "tiled";
+elseif setup.PlotLayout == "none"
+    % Return control to the invoking script or function
+    return;
+end
+
+% Check if a "SpectralUnit" field exists and if it is nonempty
+if ~isfield(setup, "SpectralUnit") || strtrim(setup.SpectralUnit) == ""
+    setup.SpectralUnit = "power";
+end
+
+% Check if a "dbReference" field exists and if it is nonempty
+if ~isfield(setup, "dbReference") || isempty(setup.dbReference)
+    setup.dbReference = NaN;
+end
+
+%% Determine spectral unit and set y-axis label
+if isnan(setup.dbReference)
+    dblab = "";
+else
+    dblab = " dB/" + num2str(setup.dbReference, "%.2e");
+end
+
+switch lower(setup.SpectralUnit)
+    case "rms"                          % RMS magnitude
+        unitlab = "Autospectrum" + dblab + " (" + setup.Unit + ", RMS)";
+    case {"pk", "0-pk", "peak"}         % 0-peak magnitude
+        unitlab = "Autospectrum" + dblab + " (" + setup.Unit + ", 0-Pk)";
+    case {"pp", "pk-pk", "peak2peak"}   % Peak-peak magnitude
+        unitlab = "Autospectrum" + dblab + " (" + setup.Unit + ", Pk-Pk)";
+    case {"asd", "psd"}                 % Power spectral density
+        unitlab = "Power Spectral Density" + dblab + " (" + setup.Unit ...
+                  + ")^2 / Hz";
+    case {"rsd", "rmssd"}               % Root mean square spectral density
+        unitlab = "Power Spectral Density" + dblab + " (" + setup.Unit ...
+                  + ") / (Hz^1/2)";
+    otherwise                           % Autospectrum / power spectrum
+        unitlab = "Autospectrum" + dblab + " (" + setup.Unit + ")^2";
+end
+
+%% Plot data
+switch setup.PlotLayout
+    % Plots stacked in one axes
+    case "stacked"
+        % Initialise figure
+        fig = figure("Color", "w");
+        ax  = axes(fig);
+	    setAxes(ax, unitlab);
+        legend(ax);
+	    
+        % Cycle through spectra
+        for col = 1:size(s, ndims(s))
+            plot(ax, f, s(:, col), "DisplayName", "signal " + col);
+        end
+
+    % Graphs tiled in one figure
+    case "tiled"
+        % Initialise figure
+        fig = figure("color", "w");
+
+        % Try to use a tiled layout (R2019a or newer)
+        try
+            tiledlayout(fig, "flow")
+
+            % Cycle through results
+            for col = 1:size(s, ndims(s))
+                % Initialise new tile
+                 ax = nexttile;
+
+                % Plot spectrograms as surfaces or spectra as plots
+		        if setup.Averaging == "none"
+                    setAxes(ax, "Time (s)");
+                    surface(f, t, squeeze(s(:, :, col)).', ...
+                            "DisplayName", "signal " + col, ...
+                            "EdgeColor","none", "FaceColor", "interp");
+                     cbar = colorbar; 
+                    title(cbar, unitlab);
                 else
-                    ax = initAxes(fig, slabel);
-		            plot(ax, f, s(:, col), DisplayName = "Signal " + col);
-			    end
-			    
-		        legend(ax);
-            end	
-    end
+                    setAxes(ax, unitlab);
+	                plot(f, s(:, col), "DisplayName", "signal " + col);
+                end
 
-    function ax = initAxes(target, label)
-	    ax = axes(target);
-	    ax.Box = "on";
-	    ax.FontName = "Times New Roman";
-        ax.Layer = "top";
-	    ax.NextPlot = "add";
-	    ax.XLabel.String = "Frequency (Hz)";
-        ax.YLabel.String = label;
-    end
+                legend(ax);
+            end
+            
+        % Use subplot if tiledlayout does not exist (R2018b or older)
+        catch
+            % Determine the number of tile rows and columns
+            rows = 1;
+            cols = size(s, ndims(s));
+
+            while rows < cols
+                rows = rows + 1;
+                cols = ceil(size(s, ndims(s)) / rows);
+            end
+
+            % Cycle through results
+            for col = 1:size(s, ndims(s))
+                % Initialise new subplot
+                ax = subplot(rows, cols, col);
+
+                % Plot spectrograms as surfaces or spectra as plots
+		        if setup.Averaging == "none"
+                    setAxes(ax, "Time (s)");
+                    surface(f, t, squeeze(s(:, :, col)).', ...
+                            "DisplayName", "signal " + col, ...
+                            "EdgeColor","none", "FaceColor", "interp");
+                    cbar = colorbar; 
+                    title(cbar, unitlab);
+                else
+                    setAxes(ax, unitlab);
+	                plot(f, s(:, col), "DisplayName", "signal " + col);
+                end
+
+                legend(ax);
+            end
+        end
+
+    % Graphs displayed in separate figures
+    case "separated"
+        % Cycle through results
+        for col = 1:size(s, ndims(s))
+            % Initialise new figure
+            figure("Color", "w");
+
+            % Plot spectrograms as surfaces or spectra as plots
+		    if setup.Averaging == "none"
+		        surface(f, t, squeeze(s(:, :, col)).', ...
+                        "DisplayName", "signal " + col, ...
+                        "EdgeColor","none", "FaceColor", "interp");
+                setAxes(gca, "Time (s)");
+                cbar = colorbar; 
+                title(cbar, unitlab);
+            else
+	            plot(f, s(:, col), "DisplayName", "signal " + col);
+                setAxes(gca, unitlab);
+		    end
+		    
+            % Set axes and show legend
+	        legend(gca);
+        end	
+end
+
+%% Local functions
+function setAxes(target, str)
+% setAxes(target, label) sets properties of axes stored in target and uses
+%   str to label y-axis.
+
+    target.Box = "on";
+    target.FontName = "Times New Roman";
+    target.Layer = "top";
+    target.NextPlot = "add";
+    target.XLabel.String = "Frequency (Hz)";
+    target.YLabel.String = str;
+end
 
 % End of main function
 end
